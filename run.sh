0c6115541cf7d2a6fe0eebc76c704c0190f7b839
leonobi4-lang <leonobi4@gmail.com>
2025-10-27 21:38:38 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
-echo "🚀 Starting photo sync at $(date '+%Y-%m-%d %H:%M:%S')"
-python3 /app/sync.py "$@"
-echo "✅ Photo sync finished at $(date '+%Y-%m-%d %H:%M:%S')"
+#!/bin/sh
+echo "🚀 Starting photo_sync job..."
+python3 /app/sync.py
+echo "✅ Done."
